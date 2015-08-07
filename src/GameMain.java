e77805d65e215edf6c799fd844ff2ab4f07b2f11
DizzyCode <DizzyCode@users.noreply.github.com>
2015-08-07 09:30:56 -0500

    Finally, some code in this repo...

---
+++ src/GameMain.java
@@ -0,0 +1,148 @@
+//Daniel Zou. Using Slick2D.
+
+package ponggame;
+import org.newdawn.slick.*;
+import org.newdawn.slick.geom.*;
+import org.newdawn.slick.gui.*;
+import java.util.Random;
+import java.awt.Font;
+
+public class GameMain extends BasicGame{
+    
+    static int width = 640;
+    static int height = 480;
+    Vector2f ballVelocity;
+    Circle ball;
+    Paddle paddlePlayer;
+    Paddle paddleAI;
+    Rectangle middleLine;
+    Random random;
+    Font scoreFont;
+    TrueTypeFont score;
+    Color darkGray;
+    int countPlayer;
+    int countAI;
+    
+    public GameMain(String title) {
+        super(title);
+    }
+    
+    public void init(GameContainer gc) {
+        gc.setShowFPS(false);
+        gc.getInput().enableKeyRepeat();
+        
+        ballVelocity = new Vector2f(0, 0);
+        ball = new Circle(width / 2, height / 2, 10);
+        paddlePlayer = new Paddle(5, height);
+        paddleAI = new Paddle(width - 15, height);
+        middleLine = new Rectangle(width / 2, 0, 5, height);
+        scoreFont = new Font("Impact", Font.PLAIN, 60);
+        score = new TrueTypeFont(scoreFont, false);
+        random = new Random();
+        darkGray = new Color(50, 50, 50);
+        
+        countPlayer = 0;
+        countAI = 0;
+        
+        if(random.nextInt(2) == 0) {
+            ballVelocity = new Vector2f(9, 3);
+        }
+        else {
+            ballVelocity = new Vector2f(9, -3);
+        }
+    }
+    
+    public void render(GameContainer gc, Graphics g) {
+        score.drawString(width / 4 - 30, 10, Integer.toString(countPlayer), darkGray);
+        score.drawString(width * 3 / 4 - 30, 10, Integer.toString(countAI), darkGray);
+        
+        g.setColor(darkGray);
+        g.fill(middleLine);
+        
+        paddlePlayer.fillPaddle(g);
+        paddleAI.fillPaddle(g);
+        
+        g.setColor(new Color(255, 160, 80));
+        g.fill(ball);   
+    }
+    
+    public void update(GameContainer gc, int delta) {
+        paddlePlayer.controlPaddle(gc, height);
+        
+        ball.setLocation(ball.getX() + ballVelocity.getX(), ball.getY() + ballVelocity.getY());
+        
+        if(ball.getMaxX() >= width + 30) {
+            countPlayer++;
+            ball.setX(width / 2);
+            ball.setY(height / 2);
+            if(random.nextInt(2) == 0) {
+                ballVelocity.set(-9, 3);
+            }
+            else {
+                ballVelocity.set(-9, -3);
+            }
+        }
+        if(ball.getMinX() <= -30) {
+            countAI++;
+            ball.setX(width / 2);
+            ball.setY(height / 2);
+            
+            if(random.nextInt(2) == 0) {
+                ballVelocity.set(9, 3);
+            }
+            else {
+                ballVelocity.set(9, -3);
+            }
+        }
+        
+        if(paddlePlayer.getIntersection(ball) != -1) {
+            float intersectPos = paddlePlayer.getIntersection(ball);
+            if(Math.abs((double)ballVelocity.y / ballVelocity.x) < 0.1) {
+                if(random.nextInt(2) == 0) {
+                    ballVelocity = new Vector2f(9, 3);
+                }
+                else {
+                    ballVelocity = new Vector2f(9, -3);
+                }
+            }
+            else if(intersectPos == 0) {
+                ballVelocity.x = -ballVelocity.getX();
+                ballVelocity.y = ballVelocity.getY() / 3;
+            }
+            else if(intersectPos == 1) {
+                ballVelocity.x = -ballVelocity.getX();
+            }
+            else if(intersectPos == 2) {
+                ballVelocity.x = -ballVelocity.getX();
+                ballVelocity.y = ballVelocity.getY() * 3;
+            }
+        }
+        else if(paddleAI.getIntersection(ball) != -1) {
+            float intersectPos = paddleAI.getIntersection(ball);
+            if(intersectPos == 0) {
+                ballVelocity.x = -ballVelocity.getX();
+                ballVelocity.y = ballVelocity.getY() / 1.5f;
+            }
+            else if(intersectPos == 1) {
+                ballVelocity.x = -ballVelocity.getX();
+            }
+            else if(intersectPos == 2) {
+                ballVelocity.x = -ballVelocity.getX();
+                ballVelocity.y = ballVelocity.getY() * 1.5f;
+            }
+        }
+        else if(ball.getY() <= 0 || ball.getY() >= height - 20) {
+            ballVelocity.y = -ballVelocity.getY();
+        }
+        
+        float yPos = ball.getCenterY();
+        paddleAI.movePaddle(yPos);
+    }
+    
+    public static void main(String[] args) throws SlickException {
+        AppGameContainer app = new AppGameContainer(new GameMain("Pong!"));
+        app.setDisplayMode(width, height, false);
+        app.setTargetFrameRate(60);
+        app.start();
+    }
+}
---
+++ src/Paddle.java
@@ -0,0 +1,72 @@
+//Daniel Zou. Using Slick2D.
+
+package ponggame;
+import org.newdawn.slick.*;
+import org.newdawn.slick.geom.*;
+
+public class Paddle {
+    
+    Rectangle top;
+    Rectangle middle;
+    Rectangle bottom;
+    
+    public Paddle(float xPos, float gameHeight) {
+        top = new Rectangle(xPos, (gameHeight / 2) - 30, 10, 30);
+        middle = new Rectangle(xPos, gameHeight / 2, 10, 30);
+        bottom = new Rectangle(xPos, (gameHeight / 2) + 30, 10, 30);
+    }
+    
+    public void controlPaddle(GameContainer gc, float gameHeight) {
+        if(gc.getInput().isKeyDown(Input.KEY_UP)) {
+            if(this.top.getMinY() > 0) {
+                this.top.setY(this.top.getY() - 7.0f);
+                this.middle.setY(this.middle.getY() - 7.0f);
+                this.bottom.setY(this.bottom.getY() - 7.0f);
+            }
+        }
+        else if(gc.getInput().isKeyDown(Input.KEY_DOWN)) {
+            if(this.bottom.getMaxY() < gameHeight) {
+                this.top.setY(this.top.getY() + 7.0f);
+                this.middle.setY(this.middle.getY() + 7.0f);
+                this.bottom.setY(this.bottom.getY() + 7.0f);
+            }
+        }
+    }
+    
+    public void movePaddle(float ballPos) {
+        if(ballPos < this.middle.getCenterY() - 7) {
+            this.top.setY(this.top.getY() - 6.0f);
+            this.middle.setY(this.middle.getY() - 6.0f);
+            this.bottom.setY(this.bottom.getY() - 6.0f);
+        }
+        else if(ballPos > this.middle.getCenterY() + 7) {
+            this.top.setY(this.top.getY() + 6.0f);
+            this.middle.setY(this.middle.getY() + 6.0f);
+            this.bottom.setY(this.bottom.getY() + 6.0f);
+        }
+    }
+    
+    public void fillPaddle(Graphics g) {
+        g.setColor(Color.white);
+        g.fill(this.top);
+        g.fill(this.middle);
+        g.fill(this.bottom);
+    }
+    
+    public float getIntersection(Shape shape) {
+        if(shape.intersects(this.middle)) {
+            return 1;
+        }
+        else if(shape.intersects(this.top)) {
+            return 0;
+        }
+        else if(shape.intersects(this.bottom)) {
+            return 2;
+        }
+        return -1;
+    }
+    
+    public float getSurfaceX() {
+        return top.getMaxX();
+    }
+}
